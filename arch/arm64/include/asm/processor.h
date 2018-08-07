664dd82a9abe1ffd082e5d89f3c9d7911006b7ee
Will Deacon <will.deacon@arm.com>
2018-08-07 13:53:41 +0100

    arm64: cpu: Move errata and feature enable callbacks closer to callers
    BugLink: https://bugs.launchpad.net/bugs/1850675
    
    The cpu errata and feature enable callbacks are only called via their
    respective arm64_cpu_capabilities structure and therefore shouldn't
    exist in the global namespace.
    
    Move the PAN, RAS and cache maintenance emulation enable callbacks into
    the same files as their corresponding arm64_cpu_capabilities structures,
    making them static in the process.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (backported from commit b8925ee2e12d1cb9a11d6f28b5814f2bfa59dce1)
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Khalid Elmously <khalid.elmously@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -243,10 +243,6 @@ static inline void spin_lock_prefetch(const void *ptr)
 
 #endif
 
-void cpu_enable_pan(const struct arm64_cpu_capabilities *__unused);
-void cpu_enable_cache_maint_trap(const struct arm64_cpu_capabilities *__unused);
-void cpu_clear_disr(const struct arm64_cpu_capabilities *__unused);
-
 /* Userspace interface for PR_SVE_{SET,GET}_VL prctl()s: */
 #define SVE_SET_VL(arg)	sve_set_current_vl(arg)
 #define SVE_GET_VL()	sve_get_current_vl()
