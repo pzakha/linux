c5afd339e48c6be73fab925992cc310535630602
Paolo Pisati <paolo.pisati@canonical.com>
2019-05-21 10:20:47 +0000

    Revert "PCI: qcom: add runtime pm support to pcie_port"
    BugLink: https://bugs.launchpad.net/bugs/1832012
    
    This reverts commit d88445937b9179cbc5f586de3f4043d1e73cecda.
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/pci/dwc/pcie-qcom.c
+++ drivers/pci/dwc/pcie-qcom.c
@@ -1144,9 +1144,6 @@ static int qcom_pcie_host_init(struct pcie_port *pp)
 	struct qcom_pcie *pcie = to_qcom_pcie(pci);
 	int ret;
 
-
-	pm_runtime_get_sync(pci->dev);
-
 	qcom_ep_reset_assert(pcie);
 
 	ret = pcie->ops->init(pcie);
@@ -1183,7 +1180,6 @@ err_disable_phy:
 	phy_power_off(pcie->phy);
 err_deinit:
 	pcie->ops->deinit(pcie);
-	pm_runtime_put_sync(pci->dev);
 
 	return ret;
 }
@@ -1272,7 +1268,6 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	if (!pci)
 		return -ENOMEM;
 
-	pm_runtime_enable(dev);
 	pci->dev = dev;
 	pci->ops = &dw_pcie_ops;
 	pp = &pci->pp;
@@ -1322,7 +1317,6 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 				       "qcom-pcie-msi", pp);
 		if (ret) {
 			dev_err(dev, "cannot request msi irq\n");
-			pm_runtime_disable(&pdev->dev);
 			return ret;
 		}
 	}
@@ -1336,7 +1330,6 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	ret = dw_pcie_host_init(pp);
 	if (ret) {
 		dev_err(dev, "cannot initialize host\n");
-		pm_runtime_disable(&pdev->dev);
 		return ret;
 	}
 
