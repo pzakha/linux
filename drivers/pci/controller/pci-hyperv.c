f89de63238639a51f99507b87be141def2c11b02
Haiyang Zhang <haiyangz@microsoft.com>
2019-10-16 22:28:00 +0200

    PCI: hv: Use bytes 4 and 5 from instance ID as the PCI domain numbers
    BugLink: https://bugs.launchpad.net/bugs/1847139
    
    As recommended by Azure host team, the bytes 4, 5 have more uniqueness
    (info entropy) than bytes 8, 9 so use them as the PCI domain numbers.
    
    On older hosts, bytes 4, 5 can also be used -- no backward compatibility
    issues are introduced and the chance of collision is greatly reduced.
    
    In the rare cases of collision, the driver code detects and finds
    another number that is not in use.
    
    Suggested-by: Michael Kelley <mikelley@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Acked-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit f73f8a504e27959576a2f4d85182202561e426f2)
    [marcelo.cerri@canonical.com: adjustments due to the context difference]
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/pci/controller/pci-hyperv.c
+++ drivers/pci/controller/pci-hyperv.c
@@ -2559,8 +2559,8 @@ static int hv_pci_probe(struct hv_device *hdev,
 	 * between domains derived from these instance IDs in the same
 	 * VM.
 	 */
-	hbus->sysdata.domain = hdev->dev_instance.b[9] |
-			       hdev->dev_instance.b[8] << 8;
+	hbus->sysdata.domain = hdev->dev_instance.b[4] |
+			       hdev->dev_instance.b[5] << 8;
 
 	hbus->hdev = hdev;
 	refcount_set(&hbus->remove_lock, 1);
